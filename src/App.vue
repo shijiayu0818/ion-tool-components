ef2a79ddb5d26f7d76375c6126d9eae713519dd5
jiashi <jiayu.shi.ext@nokia-sbell.com>
2021-07-01 11:28:49 +0800

    未增加eslint

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
  * @Author: Shi Shi
  * @Date: 2021-06-29 16:06:56
  * @LastEditors: Shi Shi
- * @LastEditTime: 2021-07-01 10:20:08
+ * @LastEditTime: 2021-07-01 11:25:21
 -->
 <template>
   <div>
@@ -14,11 +14,8 @@
 </template>
 
 <script>
-// import Main from './components/Main'
-// import First from './components/First'
 import SjySecend from './components/sjySecend/index.js'
 import First from './components/first/index.js'
-// import First from '../index.js'
 export default {
   data () {
     return {
